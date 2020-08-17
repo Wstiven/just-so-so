c8288d8b0ac1c2d3c4926b05d640ce12cbcb68aa
Wstiven <870915944@qq.com>
2020-08-17 21:47:43 +0800

    [√]修改目录结构

--- myStudy/src/Thread/ThreadPoolDemo.java
+++ myStudy/src/Thread/ThreadPoolDemo.java
@@ -1,3 +1,5 @@
+package Thread;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
--- myStudy/src/test1.java
+++
@@ -1,5 +0,0 @@
-public class test1 {
-        public void main(String args []){
-            System.out.println(11);
-        }
-}
