871a2af53d5c8dbde8609b45383294fb2e7fa001
Wstiven <870915944@qq.com>
2020-09-22 16:45:54 +0800

    [+]日常提交

--- src/dataStructure/chuan/chuan.java
+++ src/dataStructure/chuan/chuan.java
@@ -1,26 +1,26 @@
 package dataStructure.chuan;
 
-public class chuan extends chuanAbstract{
+public class chuan extends chuanAbstract {
 
 
     @Override
     public void String(int n) {//构造一个串对象
-        this.maxSize=n;
-        this.chars=new char[n];
-        this.length=0;
+        this.maxSize = n;
+        this.chars = new char[n];
+        this.length = 0;
     }
 
     @Override//基本算法与String类的compareTo一致
     public int compare(String s) {
-        int i=0;
-        while(this.chars[i]==s.charAt(i)&&i<this.chars.length&&i<s.length()){
+        int i = 0;
+        while (this.chars[i] == s.charAt(i) && i < this.chars.length && i < s.length()) {
             i++;
         }
-        if(i==this.length&&i==s.length()){
-            return  0;//完全相等
-        }else if(i==s.length()&&i<length){
+        if (i == this.length && i == s.length()) {
+            return 0;//完全相等
+        } else if (i == s.length() && i < length) {
             return 1;//包含关系
-        }else{
+        } else {
             return -1;//不相等
         }
     }
@@ -31,33 +31,33 @@ public class chuan extends chuanAbstract{
     */
     @Override
     public void concat(String s) {//总体可看做先判断原串的最大长度是否能容纳下新增的串，如不能先进行一个扩容操作，之后进行一个串内容转换
-        if(this.maxSize<this.length+s.length()){//长度扩容
+        if (this.maxSize < this.length + s.length()) {//长度扩容
             char[] a = new char[this.length];
-            for(int i=0;i<chars.length;i++){
-                a[i]=chars[i];
+            for (int i = 0; i < chars.length; i++) {
+                a[i] = chars[i];
             }
-            this.maxSize=this.length+s.length();
-            this.chars=new char[maxSize];
+            this.maxSize = this.length + s.length();
+            this.chars = new char[maxSize];
 
-            for(int i=0;i<a.length;i++){//恢复当前串的原始状态
-                this.chars[i]=a[i];
+            for (int i = 0; i < a.length; i++) {//恢复当前串的原始状态
+                this.chars[i] = a[i];
             }
         }
 
-        for(int i=0;i<s.length();i++){//将追加的字符串追加到原字符串末尾
-            this.chars[this.length]=s.charAt(i);
+        for (int i = 0; i < s.length(); i++) {//将追加的字符串追加到原字符串末尾
+            this.chars[this.length] = s.charAt(i);
             this.length++;
         }
     }
 
     @Override
     public String subString(int pos, int len) {
-        if(pos+len>this.length){//如果获取的子串起始位置与结束位置之和大于串长度，返回一个空字符串
+        if (pos + len > this.length) {//如果获取的子串起始位置与结束位置之和大于串长度，返回一个空字符串
             return null;
         }
         StringBuffer a = new StringBuffer();
-        for(int i=0;i<len;i++){
-            a.append(this.chars[i+pos]);
+        for (int i = 0; i < len; i++) {
+            a.append(this.chars[i + pos]);
         }
         return a.toString();
     }
